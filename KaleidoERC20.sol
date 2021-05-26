dfa7dc7f10b2aff49f61b67c3d8f4b6945ba00d4
Dipesh-dev08 <70092583+Dipesh-dev08@users.noreply.github.com>
2021-05-26 11:53:59 +1000

    Update KaleidoERC20.sol

--- KaleidoERC20.sol
+++ KaleidoERC20.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.6.2;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 contract Supplier is ERC20 {
     constructor(string memory SupplierID, string memory Name, uint8 ProductID, uint8 ProductName) ERC20(name, ProductID) public {
         _setupDecimals(decimals);
